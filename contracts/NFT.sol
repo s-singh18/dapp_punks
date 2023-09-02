ba0e3cdd2c01a2ee406cf3214cf4d3a20785658e
s-singh18 <65569092+s-singh18@users.noreply.github.com>
2023-09-01 23:40:43 -0700

    Finished NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,10 +5,16 @@ import "./ERC721Enumerable.sol";
 import "./Ownable.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
     uint256 public cost;
     uint256 public maxSupply;
     uint256 public allowMintingOn;
-    string public baseURI;
+
+    event Mint(uint256 amount, address minter);
+    event Withdraw(uint256 amount, address owner);
 
     constructor(
         string memory _name,
@@ -22,5 +28,55 @@ contract NFT is ERC721Enumerable, Ownable {
         maxSupply = _maxSupply;
         allowMintingOn = _allowMintingOn;
         baseURI = _baseURI;
+    }
+
+    function mint(uint256 _mintAmount) public payable {
+        // Create a token
+        require(block.timestamp >= allowMintingOn);
+
+        require(_mintAmount > 0);
+
+        require(msg.value >= cost * _mintAmount);
+
+        uint256 supply = totalSupply();
+
+        require(supply + _mintAmount <= maxSupply);
+
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
+
+        emit Mint(_mintAmount, msg.sender);
+    }
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view virtual override returns (string memory) {
+        require(_exists(_tokenId), "token does not exist");
+        return (
+            string(
+                abi.encodePacked(baseURI, _tokenId.toString(), baseExtension)
+            )
+        );
+    }
+
+    function walletOfOwner(
+        address _owner
+    ) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+
+        (bool success, ) = payable(msg.sender).call{value: balance}("");
+        require(success);
+
+        emit Withdraw(balance, msg.sender);
     }
 }
