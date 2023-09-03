3388ae493f49c520f77184f605ffb6c6b553653f
s-singh18 <65569092+s-singh18@users.noreply.github.com>
2023-09-02 17:22:23 -0700

    Add website minting

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,124 @@
-import { useEffect, useState } from 'react'
-import { Container } from 'react-bootstrap'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Countdown from "react-countdown";
+import { ethers } from "ethers";
+
+import preview from "../preview.png";
 
 // Components
-import Navigation from './Navigation';
-import Loading from './Loading';
+import Navigation from "./Navigation";
+import Data from "./Data";
+import Mint from "./Mint";
+import Loading from "./Loading";
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import NFT_ABI from "../abis/NFT.json";
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from "../config.json";
 
 function App() {
-  const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [provider, setProvider] = useState(null);
+  const [nft, setNFT] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(true)
+  const [account, setAccount] = useState(null);
+
+  const [revealTime, setRevealTime] = useState(0);
+  const [maxSupply, setMaxSupply] = useState(0);
+  const [totalSupply, setTotalSupply] = useState(0);
+  const [cost, setCost] = useState(0);
+  const [balance, setBalance] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(true);
 
   const loadBlockchainData = async () => {
     // Initiate provider
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const nft = new ethers.Contract(
+      config[31337].nft.address,
+      NFT_ABI,
+      provider
+    );
+    setNFT(nft);
 
     // Fetch accounts
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    const account = ethers.utils.getAddress(accounts[0])
-    setAccount(account)
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    // Fetch Countdown
+    const allowMintingOn = await nft.allowMintingOn();
+    setRevealTime(allowMintingOn.toString() + "000");
 
-    setIsLoading(false)
-  }
+    const maxSupply = await nft.maxSupply();
+    setMaxSupply(maxSupply);
+
+    const totalSupply = await nft.totalSupply();
+    setTotalSupply(totalSupply);
+
+    const cost = await await nft.cost();
+    setCost(cost);
+
+    const balance = await nft.balanceOf(account);
+    setBalance(balance);
+
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     if (isLoading) {
-      loadBlockchainData()
+      loadBlockchainData();
     }
   }, [isLoading]);
 
-  return(
+  return (
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className="my-4 text-center">Dapp Punks</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
+          <Row>
+            {balance > 0 ? (
+              <img
+                src={`https://gateway.pinata.cloud/ipfs/QmQPEMsfd1tJnqYPbnTQCjoa8vczfsV1FmqZWgRdNQ7z3g/${balance.toString()}.png`}
+                alt="Open Punk"
+                width="400px"
+                height="400px"
+              />
+            ) : (
+              <img src={preview} alt="" />
+            )}
+            <Col></Col>
+            <Col>
+              <div className="my-4 text-center">
+                <Countdown date={parseInt(revealTime)} className="h2" />
+              </div>
+              <Data
+                maxSupply={maxSupply}
+                totalSupply={totalSupply}
+                cost={cost}
+                balance={balance}
+              />
+              <Mint
+                provider={provider}
+                nft={nft}
+                cost={cost}
+                setIsLoading={setIsLoading}
+              />
+            </Col>
+          </Row>
         </>
       )}
     </Container>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Data.js
@@ -0,0 +1,20 @@
+import { ethers } from "ethers";
+
+const Data = ({ maxSupply, totalSupply, cost, balance }) => {
+  return (
+    <div>
+      <p>
+        <strong>Available to Mint:</strong> {maxSupply - totalSupply}
+      </p>
+      <p>
+        <strong>Cost to Mint:</strong> {ethers.utils.formatUnits(cost, "ether")}
+        ETH
+      </p>
+      <p>
+        <strong>You own:</strong> {balance.toString()}
+      </p>
+    </div>
+  );
+};
+
+export default Data;
